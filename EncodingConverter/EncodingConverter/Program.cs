f8fb4b56cf2280721cbf334ed14787154a5c0c64
Gergő Ádám <rapture047@gmail.com>
2019-06-15 16:39:53 +0200

    Encoding converter done

---
+++ EncodingConverter/EncodingConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace EncodingConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("../../../../../.gitignore"))
+            {
+                using(FileStream fs = new FileStream("../../../../../.gitignore_utf7.txt",FileMode.Create, FileAccess.Write))
+                {
+                    using(StreamWriter sw = new StreamWriter(fs,Encoding.UTF7))
+                    {
+                        string content = sr.ReadToEnd();
+                        sw.Write(content);
+                    }
+                }
+            }
+        }
+    }
+}
