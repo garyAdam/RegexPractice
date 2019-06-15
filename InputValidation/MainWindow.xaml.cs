3a2991236d88c777efd025ac65c4232e6452e8cf
Gergő Ádám <rapture047@gmail.com>
2019-06-15 14:41:00 +0200

    Add project files.

---
+++ InputValidation/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InputValidation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecuteSave(object sender, CanExecuteRoutedEventArgs e)
+        {
+            
+        }
+
+        private void CommandBinding_ExecutedSave(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
